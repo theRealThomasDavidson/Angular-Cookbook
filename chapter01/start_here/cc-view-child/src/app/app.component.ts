e11fe34b154cd23e06d7baf46a127d18816d2464
theRealThomasDavidson <davidson.thomasj@gmail.com>
2022-10-18 16:11:29 -0400

    Finished: Accessing a child componenet...using ViewChild||Starting: Creating your first dynamic...

--- chapter01/start_here/cc-view-child/src/app/app.component.ts
+++ chapter01/start_here/cc-view-child/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { GalleryComponent } from './components/gallery/gallery.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,12 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cc-view-child';
-
+  @ViewChild(GalleryComponent) gallery
   addNewPicture() {
+    this.gallery.pictures.unshift(this.gallery.generateImage())
     console.log('added new picture');
   }
 
   removeFirstPicture() {
+    this.gallery.pictures.shift()
     console.log('removed first picture');
   }
 }
