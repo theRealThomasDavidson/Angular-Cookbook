d775ddeeb2a869f8f3260c9594e6fe326f928f33
theRealThomasDavidson <davidson.thomasj@gmail.com>
2022-10-18 14:04:44 -0400

    WIP ch1

--- chapter01/start_here/cc-services/src/app/notifications-manager/notifications-manager.component.ts
+++ chapter01/start_here/cc-services/src/app/notifications-manager/notifications-manager.component.ts
@@ -1,30 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { NotificationsService } from '../services/notifications.service';
+import { first } from 'rxjs/operators';
 @Component({
   selector: 'app-notifications-manager',
   templateUrl: './notifications-manager.component.html',
   styleUrls: ['./notifications-manager.component.scss']
 })
 export class NotificationsManagerComponent implements OnInit {
-  notificationsCount = 0
-  constructor() { }
+  notificationsCount$: Observable<number>
+  constructor(private notificationsService: NotificationsService) {
+
+  }
 
   ngOnInit(): void {
+    this.notificationsCount$ = this.notificationsService.count$
   }
 
   addNotification() {
-    this.notificationsCount++;
+    this.getCountValue((countVal) => {
+    this.notificationsService.setCount(++countVal)
+    });
   }
 
   removeNotification() {
-    if (this.notificationsCount == 0) {
-      return;
-    }
-    this.notificationsCount--;
+    //if (this.notificationsCount == 0) {
+    //  return;
+    //}
+    //this.notificationsCount--;
   }
 
   resetCount() {
-    this.notificationsCount = 0;
+    //this.notificationsCount = 0;
   }
+  getCountValue(callback){
+    this.notificationsCount$.pipe(first()).subscribe(callback)
+  }
+
 
 }
