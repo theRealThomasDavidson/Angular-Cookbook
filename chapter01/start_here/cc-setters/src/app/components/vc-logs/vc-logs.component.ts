b3b02cfa86d0de4f9e3f5352e4df7f04a9107748
theRealThomasDavidson <davidson.thomasj@gmail.com>
2022-10-18 14:56:51 -0400

    Finished: Using Setters...||Starting Using ngOnChanges...

--- chapter01/start_here/cc-setters/src/app/components/vc-logs/vc-logs.component.ts
+++ chapter01/start_here/cc-setters/src/app/components/vc-logs/vc-logs.component.ts
@@ -6,7 +6,25 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./vc-logs.component.scss']
 })
 export class VcLogsComponent implements OnInit {
-  @Input() vName;
+
+  logs: string[] = [];
+  _vName: string
+  @Input()
+  get vName(){
+    return this._vName
+  }
+  set vName(name: string){
+    if (!name){
+      return
+    }
+    if (!this._vName){
+      this.logs.push(`initial version is ${name.trim()}`)
+    }
+    else{
+      this.logs.push(`Version converted from ${this._vName.trim()} to ${name.trim()}`)
+    }
+    this._vName = name
+  }
   constructor() { }
 
   ngOnInit(): void {
