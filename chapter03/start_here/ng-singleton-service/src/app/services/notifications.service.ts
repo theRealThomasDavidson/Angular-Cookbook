f9c3175a6b6e51c4fc4cdb516c4e7f3c74b2dada
theRealThomasDavidson <davidson.thomasj@gmail.com>
2022-10-20 12:43:11 -0400

    finished: Creating a singleton|| Starting: Creating a ... forRoot()

--- chapter03/start_here/ng-singleton-service/src/app/services/notifications.service.ts
+++ chapter03/start_here/ng-singleton-service/src/app/services/notifications.service.ts
@@ -1,12 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, SkipSelf, Optional } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
-@Injectable()
+@Injectable(
+  {
+    providedIn: 'root'
+  }
+)
 export class NotificationsService {
   private count: BehaviorSubject<number> = new BehaviorSubject<number>(0);
 
   count$: Observable<number> = this.count.asObservable();
-  constructor() {}
+  constructor(@Optional() @SkipSelf() existingService: NotificationsService) {
+    if (existingService) {
+      throw Error ('The service has already been provided in the app.' +
+      ' Avoid providing it again in child modules');
+      }
+  }
 
   setCount(countVal) {
     this.count.next(countVal);
