d6540a1b7e9f85662003d349575d6f0c29359b95
theRealThomasDavidson <davidson.thomasj@gmail.com>
2022-10-25 15:51:00 -0400

    finished stagger starting callbacks

--- chapter04/start_here/animating-lists/src/app/app.module.ts
+++ chapter04/start_here/animating-lists/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
@@ -13,6 +13,7 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ chapter04/start_here/animating-lists/src/app/constants/animations.ts
@@ -0,0 +1,75 @@
+import {
+  trigger,
+  style,
+  animate,
+  transition,
+  stagger,
+  query,
+ } from '@angular/animations';
+export const ANIMATIONS = {
+  LIST_ANIMATION: trigger(
+    'listAnimation',
+    [
+      transition(
+        '* <=> *',
+        [
+        query(
+          ':enter',
+          [
+            style(
+              {
+                opacity: 0
+              }
+            ),
+            stagger(
+              100,
+              [
+                animate('0.5s ease',
+                  style(
+                    {
+                      opacity: 1
+                    }
+                  )
+                ),
+              ]
+            )
+          ],
+          {
+            optional:true
+          }
+        )
+      ],
+    ),
+
+    query(
+      ':leave',
+      [
+      transition(
+        '* => void',
+        [
+          style
+          (
+            {
+              opacity: 1
+            }
+          ),
+          stagger(
+            100,
+            [
+              animate(
+                '0.5s ease',
+                style(
+                  {
+                    opacity: 0
+                  }
+                )
+              )
+            ]
+          )
+        ]
+      )
+    ], {
+      optional:true
+    }
+  )]
+)}
